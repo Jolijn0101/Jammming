6170dd93b0e8f722ca93339f686d1b3b23e98a5e
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2022-06-16 14:50:58 +0200

    login text in center

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,12 +9,14 @@ function Login() {
 
   return (
     <div id="login">
-      <h1>
-        Welcome by Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <p>The web-app that makes the creation of spotify playlists easy!</p>
-      <p>Login for making a new playlist.</p>
-      <button onClick={toggleLogin}>Login to Spotify</button>
+      <div id="text-div">
+        <h1>
+          Welcome by Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <p>The web-app that makes the creation of spotify playlists easy!</p>
+        <p>Login for making a new playlist.</p>
+        <button onClick={toggleLogin}>Login to Spotify</button>
+      </div>
     </div>
   );
 }
